5c86e71cb3fdc95673a780744600b527e80476ed
eduardolopes00 <eduardo.toworkstudy@gmail.com>
2022-02-09 16:04:40 -0300

    Creating logout routine to be executed when error 401

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,5 +1,5 @@
 import { useContext, useEffect } from "react"
-import { AuthContext } from "../contexts/AuthContext"
+import { AuthContext, signOut } from "../contexts/AuthContext"
 import { api } from "../services/api";
 
 export default function Dashboard() {
@@ -7,13 +7,12 @@ export default function Dashboard() {
 
 
     useEffect(() => {
-        
-
-        
             api.get('/me').then(response => {
                 const { email, permissions, roles } = response.data
 
                 console.log(email, permissions, roles)
+            }).catch(error => {
+                signOut();
             })
         
 
