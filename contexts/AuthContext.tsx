a17e8e0de4b2a60672412074f78574ea578f83f8
eduardolopes00 <eduardo.toworkstudy@gmail.com>
2022-02-09 12:21:57 -0300

    Adding the user token to the cookies

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -1,6 +1,7 @@
-import { createContext, ReactNode, useState } from 'react';
+import { createContext, ReactNode, useEffect, useState } from 'react';
 import { api } from '../services/api';
 import Router from 'next/router'
+import { parseCookies, setCookie } from 'nookies'
 
 type User = {
     email: string;
@@ -30,6 +31,20 @@ export function AuthProvider({children}: AuthProviderProps) {
     const [user, setUser] = useState<User>()
     const isAuthenticated = false;
 
+    useEffect(() => {
+        const { 'nextauth.token': token } = parseCookies();
+
+        if (token) {
+
+            api.get('/me').then(response => {
+                const { email, permissions, roles } = response.data
+
+                setUser({email, permissions, roles});
+            })
+        }
+
+    }, [])
+
     async function signIn({email, password}: SignInCredentials): Promise<void> {
         try {
             const response = await api.post('sessions', {
@@ -39,11 +54,23 @@ export function AuthProvider({children}: AuthProviderProps) {
 
             const { token, refreshToken, permissions, roles } = response.data;
 
+            setCookie(undefined, 'nextauth.token', token, {
+                maxAge: 60 * 60 * 24 * 30, // 30 days
+                path: '/' //this propertie indicates what routes the cookie will be avaiable. When the value is just a slash, it means that all the routes in the app wll have access.
+            })
+            
+            setCookie(undefined, 'nextauth.refreshToken', refreshToken, { //the params is: context (usefull when tke cookie is filled in serverSide), cookieName, cookieValue, cookieOptions
+                maxAge: 60 * 60 * 24 * 30, // 30 days
+                path: '/' //this propertie indicates what routes the cookie will be avaiable. When the value is just a slash, it means that all the routes in the app wll have access.
+            })
+
             setUser({
                 email,
                 permissions,
                 roles
             })
+
+            api.defaults.headers['Authorization'] = `Bearer ${token}`
 
             Router.push('/dashboard')
             
